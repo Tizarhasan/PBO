de7dfaa34a620c2eb94477fd682a34b7de7b477b
tizarhasan <tizarhasan@MacBook-Pro.local>
2025-03-29 15:24:38 +0700

    Pemograman Berorientasi Objek

---
+++ src/pertemuan2/main.java
@@ -0,0 +1,12 @@
+package pertemuan2;
+
+import pertemuan2.Ball;
+
+public class main {
+	public static void main (String[] args) {
+		
+		Ball myBall1 = new Ball("red", 12, true);
+		
+		Ball myBall2 = new Ball("blue", 12, false);
+	}
+}
